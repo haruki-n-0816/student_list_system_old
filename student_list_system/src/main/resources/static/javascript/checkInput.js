39cf3189cc5de3cb3b4707201ebc50d7ce2b4564
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-03-09 17:53:23 +0900

    バリデーション実装

--- student_list_system/src/main/resources/static/javascript/checkInput.js
+++ student_list_system/src/main/resources/static/javascript/checkInput.js
@@ -1,39 +1,21 @@
-// 引数で指定されたパスでサブミットする
-function formSubmit(path){
-    if(!path){
-        alert('パスを指定してください');
-        return;
-    }
-    let form = document.getElementsByTagName('form')[0];
-    if(!form){
-        alert('フォームが取得できませんでした');
-        return;
-    }
-    form.action=path;
-    form.method="post";
-    form.submit();
-}
-// 入力画面の入力値のチェックを行った後で、
-// 引数で指定されたパスでサブミットする
-function checkInputAndSubmit(path){
-    if(!checkInput()){
-        return;
-    }
-    formSubmit(path);
-}
-// 入力画面の入力値のチェックを行う
-function checkInput(){
-    //名前の未入力チェック
-    let nameElem = document.getElementById('name');
-    if(nameElem.value === ''){
-        alert('メールアドレスを入力してください。');
-        nameElem.focus();
-        return false;
-    }
-    //名前の全角入力チェック
-    if(!nameElem.value.match(/[^\x01-\x7E]/)){
-        alert('メールアドレスは半角で入力してください。')
-        nameElem.focus();
-        return false;
-    }
-}
+window.onload = function () {
+
+    const reg = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/;
+
+    btnSubmit.disabled = true;
+    inputMail.addEventListener('input', function (event) {
+        /*入力値チェック*/
+        var message = document.getElementById("message");
+        
+        if (inputMail.value == "") {
+            message.innerHTML = "メールアドレスが未入力です。";
+            btnSubmit.disabled = true;
+        } else if (!reg.test(inputMail.value)) {
+            message.innerHTML = "メールアドレスの形式が不正です。";
+            btnSubmit.disabled = true;
+        } else {
+            message.innerHTML = "";
+            btnSubmit.disabled = false;
+        }
+    });
+};
